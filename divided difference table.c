d564fb5d49d8f9d1b75ec2702079c587e4b8ff2a
XapkotaVeeru <111596777+XapkotaVeeru@users.noreply.github.com>
2024-01-16 08:56:04 +0545

    Add files via upload

---
+++ divided difference table.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define SIZE 50
+int main()
+{
+    int n,i,j;
+    float f[SIZE][SIZE],x[SIZE];
+    printf("Enter the degree of data:");
+    scanf("%d",&n);
+    printf("Enter the datas\n");
+    for(i=0;i<=n;i++)
+    {
+        printf("x[%d]=",i);
+        scanf("%f",&x[i]);
+        printf("f[%d]=",i);
+        scanf("%f",&f[0][i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<=n-i;j++)
+        {
+            f[i][j]=(f[i-1][j+1]-f[i-1][j])/(x[i+j]-x[j]);
+        }
+    }
+    for(j=0;j<=n;j++)
+    {
+        printf("%f\n",x[j]);
+        for(i=0;i<=n-j;i++)
+        {
+            printf("%f\t",f[i][j]);
+        }
+    }
+    return 0;
+}
